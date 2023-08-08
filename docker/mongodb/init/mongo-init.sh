325fb400279f123b9ddb541103d69952fce7b16a
TsarS <s.tsarikov@gmail.com>
2023-08-08 16:54:47 +0300

    init with init.sh

---
+++ docker/mongodb/init/mongo-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+mongosh <<EOF
+use admin
+db.createUser(
+  {
+    user: "${MONGO_INITDB_ROOT_USERNAME}",
+    pwd: "${MONGO_INITDB_ROOT_PASSWORD}",
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
+  }
+)
+
+use ${MONGO_INITDB_DATABASE}
+db.createCollection("users")
+db.users.insert({"name": "john"})
+EOF
