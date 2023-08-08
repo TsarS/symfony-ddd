08499c096745d765ea1d4faa979df5bd743b4673
TsarS <s.tsarikov@gmail.com>
2023-08-08 23:31:00 +0300

    postgres migrations added

---
+++ src/User/Domain/Entity/User.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Domain\Entity;
+
+
+final class User
+{
+
+
+    public function __construct(
+        private string $email,
+        private string $id,
+        private string $password)
+    {
+
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function getRoles(): array
+    {
+        return [
+            'ROLE_USER',
+        ];
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+
+}
